cd2ec43e1cb77d3ea29abf37c8a3020c13369b5f
papergliff <papergliff@gmail.com>
2018-07-10 10:42:57 +0300

    refactor stateless components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
-const Option = props => {
-  return (
-    <div>
-      <p>{props.option}</p>
-      <button 
-        onClick={e => {
-          props.handleDeleteOption(props.option)
-        }}
-      >
-        remove
-      </button>
-    </div>
-  ) 
-}
+const Option = props => (
+  <div>
+    <p>{props.option}</p>
+    <button 
+      onClick={e => {
+        props.handleDeleteOption(props.option)
+      }}
+    >
+      remove
+    </button>
+  </div>
+)
 
 export default Option
