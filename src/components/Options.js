d6c0d73e6a33810ba7158a1ae8b2507d804aa54f
papergliff <papergliff@gmail.com>
2018-07-12 22:14:01 +0300

    Style whole app, add mobile support

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,17 +3,21 @@ import Option from './Option'
 
 const Options = props => (
   <div>
-    <button 
-      className="button button--link"
-      onClick={props.handleDeleteOptions}
-    >
-      Remove all
-    </button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
-    {props.options.map(option => (
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove all
+      </button>
+    </div>
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+    {props.options.map((option, index) => (
       <Option 
           key={option} 
           option={option}
+          count={index + 1}
           handleDeleteOption={props.handleDeleteOption}
         />
       ))
